012ccdc55f8ff952d09122cc65e294886795ca1f
anonymousbyte-s <td0984825@gmail.com>
2026-09-02 23:27:47 -0700

    Added battery charge limits of 40-80%

--- programs/tlp.nix
+++ programs/tlp.nix
@@ -18,8 +18,8 @@
       CPU_ENERGY_PERF_POLICY_ON_SAV = "power";
 
       # Optional helps save long term battery health
-      #START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
-      #STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 80;
     };
   };
 }
