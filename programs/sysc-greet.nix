7ffa4efb801afb155606b46ebc1b3390a283a0d8
anonymousbyte-s <td0984825@gmail.com>
2026-09-02 18:04:01 -0700

    Removed some old config lines from sysc-greet.nix Removed tuigreet.nix as it was replaces by sysc-greet.nix

--- programs/sysc-greet.nix
+++ programs/sysc-greet.nix
@@ -3,17 +3,11 @@
 {
   services.sysc-greet = {
     enable = true;
-    compositor = "sway";  # or "cagebreak", "sway", "hyprland" (deprecated)
+    compositor = "sway"; # or "cagebreak", "sway", "hyprland" (deprecated)
   };
 
   services.sysc-greet.settings.default_session = {
     command = "sway";
     user = "tom";
   };
-
-  # Optional: Set initial session for auto-login
-  #services.sysc-greet.settings.initial_session = {
-  #  command = "sway";
-  #  user = "tom";
-  #};
 }
